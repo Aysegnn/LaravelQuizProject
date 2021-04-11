9a2f6fe9745e608e4f7e6f9b5168f509eb90fbb0
Ayşe <ayse.gonen@kc.com.tr>
2021-04-11 22:15:35 +0300

    Migration ,Seed ve Factory Kullanımı

--- database/seeders/QuizSeeder.php
+++ database/seeders/QuizSeeder.php
@@ -5,18 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
-class DatabaseSeeder extends Seeder
+class QuizSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            UserSeeder::class,
-            QuizSeeder::class,
-        ]);
+        \App\Models\Quiz::factory(10)->create();
     }
 }
