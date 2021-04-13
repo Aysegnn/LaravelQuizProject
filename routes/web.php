3b3cb04ac85ec2df1d4d70d5056be374823be968
Ayşe <ayse.gonen@kc.com.tr>
2021-04-13 10:45:52 +0300

    Quiz Silme İşlemi

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,6 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 
 Route::group(['middleware'=>['auth','isAdmin'],'prefix'=>'admin'],function(){
+    Route::get('quizzes/{id}',[QuizController::class,'destroy'])->whereNumber('id')->name('quizzes.destroy');
     Route::resource('quizzes',QuizController::class);
 });
